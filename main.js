243e2528d427e3924e20ff1165d3c81a2d4d8e8b
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-05-10 13:12:25 +0500

    updated

--- main.js
+++ main.js
@@ -1,22 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// manufacturer name (string), a model name (string)
-function storeCarInfo(manufacturer, modelName, ...options) {
-    // Create an object 
-    let carInfo = {
-        // Setting the manufacturer property to the provided manufacturer name
-        manufacturer: manufacturer,
-        // Setting the modelName property to the provided model name
-        modelName: modelName
+function creat_car(manufacture, model, optional) {
+    return {
+        manufacture,
+        model,
+        ...optional
     };
-    // Iterate over each option provided
-    for (let option of options) {
-        // Set the property in the carInfo object corresponding to the key provided in the option tuple
-        // to the value provided in the option tuple
-        carInfo[option[0]] = option[1];
-    }
-    // Return the carInfo object containing all the car information
-    return carInfo;
 }
-let car1 = storeCarInfo('Toyota', 'Corolla', ['color', 'black'], ['year', 2017]);
-console.log(car1);
+const my_car = creat_car("Toyota", "Corolla", { Color: "Black", Year: 2020 });
+console.log(my_car);
--- main.ts
+++ main.ts
@@ -1,24 +1,14 @@
-// manufacturer name (string), a model name (string)
-function storeCarInfo(manufacturer: string, modelName: string, ...options: [string, any][]): { manufacturer: string, modelName: string, [key: string]: any } {
-    // Create an object 
-    let carInfo: { manufacturer: string,
-                    modelName: string,
-                    [key: string]: any } = {
-        // Setting the manufacturer property to the provided manufacturer name
-        manufacturer: manufacturer,
-        // Setting the modelName property to the provided model name
-        modelName: modelName
-    };
-
-    // Iterate over each option provided
-    for (let option of options) {
-        // Set the property in the carInfo object corresponding to the key provided in the option tuple
-        // to the value provided in the option tuple
-        carInfo[option[0]] = option[1];
-       }
-    // Return the carInfo object containing all the car information
-    return carInfo;
+type car={
+     manufacture:string,
+     model:string,
+     [key:string]:any;
 }
-
-let car1 = storeCarInfo('Toyota', 'Corolla', ['color', 'black'], ['year', 2017]);
-console.log(car1);
+function creat_car(manufacture:string,model:string,optional:Record<string,any>):car{
+  return{
+    manufacture,
+    model,
+    ...optional
+  }
+}
+const my_car:car=creat_car("Toyota","Corolla",{Color:"Black",Year:2020});
+console.log(my_car);
